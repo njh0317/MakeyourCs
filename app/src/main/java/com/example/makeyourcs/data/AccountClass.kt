5a82bc577a91cecf63479538ff768fe772686162
Jihae Na <njnjh0317@gmail.com>
2021-01-10 18:08:02 +0900

    [#4] modify AccountClass and add FollowClass, SubClass, WaitlistClass

--- app/src/main/java/com/example/makeyourcs/data/AccountClass.kt
+++ app/src/main/java/com/example/makeyourcs/data/AccountClass.kt
@@ -1,4 +1,10 @@
 package com.example.makeyourcs.data
 
-class AccountClass {
+data class AccountClass(val email:String, val pw:String, val sub_count:Int,
+val following_num:Int, val sub:SubClass, val follow:FollowClass, val follow_wait_list:WaitlistClass ) {
+
 }
+
+//data class Site(val url: String, val title: String) {
+//    val description = ""
+//}
---
+++ app/src/main/java/com/example/makeyourcs/data/WaitlistClass.kt
@@ -0,0 +1,4 @@
+package com.example.makeyourcs.data
+
+data class WaitlistClass(val from_account:String, val follow_date:String) {
+}
