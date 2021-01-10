5abb765edf9781b6effbc43a98c06677910b2eea
Jihae Na <njnjh0317@gmail.com>
2021-01-10 19:13:49 +0900

    [#4] test class file

--- app/src/main/java/com/example/makeyourcs/data/AccountClass.kt
+++ app/src/main/java/com/example/makeyourcs/data/AccountClass.kt
@@ -1,10 +1,5 @@
 package com.example.makeyourcs.data
 
 data class AccountClass(val email:String, val pw:String, val sub_count:Int,
-val following_num:Int, val sub:SubClass, val follow:FollowClass, val follow_wait_list:WaitlistClass ) {
-
+val following_num:Int, val sub:Array<SubClass>, val follow:Array<FollowClass>, val follow_wait_list:Array<WaitlistClass> ) {
 }
-
-//data class Site(val url: String, val title: String) {
-//    val description = ""
-//}
--- app/src/main/java/com/example/makeyourcs/data/FollowClass.kt
+++ app/src/main/java/com/example/makeyourcs/data/FollowClass.kt
@@ -1,4 +1,4 @@
 package com.example.makeyourcs.data
 
-data class FollowClass(val to_account:String, val to_account_sub:List<Int> ) {
+data class FollowClass(val to_account:String, val to_account_sub:Array<Int> ) {
 }
