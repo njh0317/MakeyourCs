689899494e5c5e2f2634f5a817f07c0b1532da4f
Jihae Na <njnjh0317@gmail.com>
2021-01-11 21:36:54 +0900

    [#4] change firebase setting

--- app/src/main/java/com/example/makeyourcs/LoginActivity.kt
+++ app/src/main/java/com/example/makeyourcs/LoginActivity.kt
@@ -2,11 +2,30 @@ package com.example.makeyourcs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.makeyourcs.data.AccountClass
+
 
 class LoginActivity : AppCompatActivity() {
+    var firestore : FirebaseFireStore? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val loginButton : Button = findViewById(R.id.login)
+        loginButton.setOnClickListener{
+            makeAccount()
+
+        }
+
+    }
+    fun makeAccount()
+    {
+        var account = AccountClass()
+        account.email = "njnjh0317@gmail.com"
+        account.pw = "1234"
+        account.userId = "njh0317"
+
 
     }
 }
--- app/src/main/java/com/example/makeyourcs/data/AccountClass.kt
+++ app/src/main/java/com/example/makeyourcs/data/AccountClass.kt
@@ -1,21 +1,21 @@
 package com.example.makeyourcs.data
 
-data class AccountClass(val userId:String? = null,
-                        val email:String? = null,
-                        val pw:String? = null,
-                        val sub_count:Int? = 0,
-                        val following_num:Int? = 0) {
-    data class SubClass(val sub_num:Int? = 0,
-                        val name:String? = null,
-                        val introduction:String? = null,
-                        val post_number:Int? = 0,
-                        val following_num:Int? = 0,
-                        val back_color:String? = "#ffffff",
-                        val profile_pic_url:String? = "default_url",
-                        val group_name:String? = null,
-                        val follower:Map<String, Boolean> = HashMap())
-    data class FollowClass(val to_account:String? = null,
-                           val to_account_sub:Map<Int, Boolean> = HashMap()
+data class AccountClass(var userId:String? = null,
+                        var email:String? = null,
+                        var pw:String? = null,
+                        var sub_count:Int? = 0,
+                        var following_num:Int? = 0) {
+    data class SubClass(var sub_num:Int? = 0,
+                        var name:String? = null,
+                        var introduction:String? = null,
+                        var post_number:Int? = 0,
+                        var following_num:Int? = 0,
+                        var back_color:String? = "#ffffff",
+                        var profile_pic_url:String? = "default_url",
+                        var group_name:String? = null,
+                        var follower:Map<String, Boolean> = HashMap())
+    data class FollowClass(var to_account:String? = null,
+                           var to_account_sub:Map<Int, Boolean> = HashMap()
 
     )
 }
