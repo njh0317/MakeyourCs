1ac8cc5bbca14fda0aa3c57762cff3f5a7c7e091
SimEuiLyang <dmlfid1348@naver.com>
2021-01-13 00:09:25 +0900

    [#8]signup_bday_page

---
+++ app/src/main/java/com/example/makeyourcs/Signup_bdayActivity.kt
@@ -0,0 +1,11 @@
+package com.example.makeyourcs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Signup_bdayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_bday)
+    }
+}
