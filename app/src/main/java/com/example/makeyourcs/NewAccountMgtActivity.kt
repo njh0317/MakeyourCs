a1e4c1d9efb14c153453db6b1a250f695619d167
SimEuiLyang <dmlfid1348@naver.com>
2021-01-17 18:20:22 +0900

    [14]부캐생성 view

---
+++ app/src/main/java/com/example/makeyourcs/NewAccountMgtActivity.kt
@@ -0,0 +1,11 @@
+package com.example.makeyourcs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewAccountMgtActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_account_mgt)
+    }
+}
