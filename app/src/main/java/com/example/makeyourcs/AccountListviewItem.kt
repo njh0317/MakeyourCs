6626e15880e1fabd378277ec44566751dc36da6b
SimEuiLyang <dmlfid1348@naver.com>
2021-01-17 17:53:32 +0900

    [14]listview 옮기기

---
+++ app/src/main/java/com/example/makeyourcs/AccountListviewItem.kt
@@ -0,0 +1,10 @@
+package com.example.makeyourcs
+
+import android.graphics.drawable.Drawable
+
+class AccountListviewItem {
+    var accountImg: Drawable? = null
+    var accountName: String? = null
+
+
+}
