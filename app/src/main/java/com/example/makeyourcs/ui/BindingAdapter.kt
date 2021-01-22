905f0cde2704ddeb3a157679f8e38706ce183c86
eun2u <hhaneunjoo@gmail.com>
2021-01-23 01:12:41 +0900

    [#11] use glide

--- app/src/main/java/com/example/makeyourcs/ui/BindingAdapter.kt
+++ app/src/main/java/com/example/makeyourcs/ui/BindingAdapter.kt
@@ -5,8 +5,15 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 @BindingAdapter("imgRes")
 fun imgLoad(view: ImageView, resId: Int){
+//    Glide.with(view.context)
+//        .load(resId)
+//        .dontTransform()
+//        .into(view)
+
     view.setImageResource(resId)
+
 }
