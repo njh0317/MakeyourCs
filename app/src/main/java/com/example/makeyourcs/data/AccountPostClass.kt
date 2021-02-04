281d45cc1afb77b3b0a15e293dbbddcf9654fba0
Jihae Na <njnjh0317@gmail.com>
2021-02-04 19:17:10 +0900

    [#22] AccountPostClass 수정

--- app/src/main/java/com/example/makeyourcs/data/AccountPostClass.kt
+++ app/src/main/java/com/example/makeyourcs/data/AccountPostClass.kt
@@ -1,9 +1,13 @@
 package com.example.makeyourcs.data
 
-data class AccountPostClass(var account_id:String? = null){
-    data class SubClass(var sub_id:String? = null){
-        data class PostIdClass(var post_id:String? = null,
-                               var order_in_feed:Int? = 0,
-                               var posting_date:String? = null)
-    }
+data class AccountPostClass(var account_id:String? = null)
+{
+    data class SubClass(var sub_id:String? = null,
+                        var postlist:MutableList<PostIdClass>?=ArrayList()
+
+    )
+    data class PostIdClass(var post_id:String? = null,
+                           var order_in_feed:Int? = 0,
+                           var posting_date:String?=null
+    )
 }
