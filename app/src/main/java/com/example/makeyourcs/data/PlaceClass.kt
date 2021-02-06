4ed2414786b840688a2b17dc332bf130de4f2b4c
Jihae Na <njnjh0317@gmail.com>
2021-02-06 15:56:57 +0900

    [#23] placeclass 수정

--- app/src/main/java/com/example/makeyourcs/data/PlaceClass.kt
+++ app/src/main/java/com/example/makeyourcs/data/PlaceClass.kt
@@ -1,3 +1,13 @@
 package com.example.makeyourcs.data
 
-data class PlaceClass(var place_name:MutableList<String>? = ArrayList())
+import com.google.type.LatLng
+
+data class PlaceClass(
+    var place_name: String,
+    var place_info: String,
+    var latitude: Float,
+    var longitude:Float
+)
+
+//var coord = LatLng(37.5670135, 126.9783740)
+//Latlng(latitude, longitude)
