7f2b930453358e8377f51ea9fc80e175f2405302
Jihae Na <njnjh0317@gmail.com>
2021-02-08 16:33:20 +0900

    [#21] delete test code

--- app/src/main/java/com/example/makeyourcs/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/makeyourcs/ui/auth/LoginActivity.kt
@@ -26,7 +26,6 @@ class LoginActivity : AppCompatActivity(), AuthListener, KodeinAware{
     private val factory: AuthViewModelFactory by instance()
     private lateinit var auth: FirebaseAuth
     private lateinit var viewModel: AuthViewModel
-    private var mBtn : Button? = null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityLoginBinding = DataBindingUtil.setContentView(
@@ -36,40 +35,7 @@ class LoginActivity : AppCompatActivity(), AuthListener, KodeinAware{
         viewModel = ViewModelProviders.of(this, factory).get(AuthViewModel::class.java)
         binding.viewmodel = viewModel
         viewModel.authListener = this
-
-        mBtn = findViewById(R.id.photo_bt) as Button
-        mBtn!!.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                System.out.println("button clicked !!")
-                val intent = Intent()
-                intent.type = "image/*"
-                intent.action = Intent.ACTION_GET_CONTENT
-                startActivityForResult(intent, 1)
-            }
-        })
-
-
     }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        // Check which request we're responding to
-        if (requestCode == 1) {
-            // Make sure the request was successful
-            if (resultCode == RESULT_OK) {
-                try {
-                    var photoUri = data?.data!! //photoUri 반환하여 뷰모델로 넘기기
-                    viewModel.setimage(photoUri)
-
-                }
-                catch(e:Exception)
-                {
-                    Log.w(TAG, "error in choose image ", e)
-
-                }
-            }
-        }
-    }
-
 
     override fun onStarted() {//대기상태, 로그인시 로딩 같은거 넣으면 됨..!!
 
